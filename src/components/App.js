e093c27b63d846a4d6c2a603c13021cf47aa49cc
Mara Rocha Fernández <maranhadisenha@gmail.com>
2022-03-03 14:56:19 +0100

    delete node-uuid dependency

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,8 @@ import '../styles/App.scss';
 //import { useEffect, useState } from 'react';
 //import callToApi from '../services/api';
 //import ls from '../services/localstorage';
-//import { v4 as uuid } from 'uuid';
 //import { Link, Route, Switch, useRouteMatch } from 'react-router-dom';
 //import PropTypes from 'prop-types';
-
-//console.log(uuid());
 
 function App() {
   /* Let's do magic! 🦄🦄🦄 */
